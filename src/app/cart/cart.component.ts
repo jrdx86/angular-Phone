a534cc7c4f9206231985196119eddc9aac72779f
jrdx86 <jrdx86@gmail.com>
2022-07-13 10:44:11 +0200

    3 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 import { Router} from '@angular/router';
 
@@ -11,17 +12,35 @@ import { Router} from '@angular/router';
 export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  /*To gather the user's name and address, use the FormBuilder group() method to set the checkoutForm property to a form model containing name and address fields.*/ 
+
+  volverHome() {
+    this.router.navigate(['']); //con esto podemos navegar a cualquier ruta
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
   
   constructor(
     private cartService: CartService,
-    private router: Router, 
+    private router: Router,
+    private formBuilder: FormBuilder, 
   ) { 
     
   }
 
   ngOnInit(): void {
   }
-  volverHome() {
-    this.router.navigate(['']); //con esto podemos navegar a cualquier ruta
-  }
+
+  
+  
 }
